c0493fbb6d2615cd2e8280797fb5c008701ad941
Niklas Gruhn <niklas.gruhn@gmail.com>
2018-01-10 17:21:59 +0100

    Add a lot of in-code documentation

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -23,8 +23,8 @@ import { decode, locate } from '../scanner.js'
 import isBoolean from 'lodash/isBoolean'
 
 const NO_LOCATION = [] // use specific array instance to guarantee equality ([] !== [] but NO_LOCATION === NO_LOCATION)
-const LOCATE_INTERVAL = 40 // 1000ms / 40ms = 25fps
-const DECODE_INTERVAL = 400
+const LOCATE_INTERVAL = 40 // milliseconds
+const DECODE_INTERVAL = 400 // milliseconds
 
 export default {
   props: {
@@ -41,36 +41,62 @@ export default {
 
   data () {
     return {
+      // current video stream instance returned by `getUserMedia`
       stream: null,
+
+      // absolute resolution of the current video stream
       streamWidth: null,
       streamHeight: null,
+
+      // whether or not first frame of current video stream has loaded yet
       streamLoaded: false,
 
+      // most recent decoded QR code content.
+      // Is only null after unpause or before init, otherwise string.
       decodeResult: null,
+
+      // array of most recent detected QR code corner coordinates
       locateResult: NO_LOCATION,
     }
   },
 
   computed: {
+    /**
+     * Conditions applying for both locating and decoding, joined in a single
+     * computed property.
+     */
     shouldScan () {
       return !this.paused && this.streamLoaded
     },
 
+    /**
+     * QR codes should only be actively decoded if the parent component has an
+     * event listener registered. Otherwise the rather expensive decoding
+     * operation is continuously executed for nothing.
+     */
     shouldDecode () {
       return this.shouldScan && this.$listeners.decode !== undefined
     },
 
+    /**
+     * Just like with `this.shouldDecode`, locating is not allowed when the
+     * parent component has no event listener registered.
+     */
     shouldLocate () {
       return this.shouldScan && this.$listeners.locate !== undefined
     },
 
+    /**
+     * Full constraints object which is passed to `getUserMedia` to request a
+     * camera stream. Properties define if a certain camera is adequate or not.
+     */
     constraints () {
-      let normalized
+      let withDefaults
 
       if (isBoolean(this.videoConstraints)) {
-        normalized = this.videoConstraints
+        withDefaults = this.videoConstraints
       } else {
-        normalized = {
+        withDefaults = {
           facingMode: { ideal: 'environment' },
           width: { min: 360, ideal: 1280, max: 1920 },
           height: { min: 240, ideal: 720, max: 1080 },
@@ -80,22 +106,42 @@ export default {
 
       return {
         audio: false,
-        video: normalized,
+        video: withDefaults,
       }
     },
   },
 
   watch: {
+    /**
+     * Propagate new decoding results to parent component. Since holding a
+     * camera for a few seconds over a QR code, produces the same result
+     * multiple times in a row, an event is only emitted when the result
+     * value actually changes. To allow re-scanning after the component is
+     * un-paused, decodeResult is set to null (see #8). Null values are never
+     * emitted though.
+     */
     decodeResult (newValue) {
       if (newValue !== null) {
         this.$emit('decode', newValue)
       }
     },
 
+    /**
+     * Propagates location of QR code when it changes. Just like with decoding
+     * results, this event is only emitted when the detected location changes.
+     *
+     * While a QR code is actually in sight, position changes are detected
+     * nearly each scanned frame anyway. While no QR code is detected though,
+     * this makes sure that empty results are only emitted once.
+     */
     locateResult (newValue) {
       this.$emit('locate', newValue)
     },
 
+    /**
+     * Automatically freezes the video stream when conditions for the scanning
+     * process are not fullfilled anymore.
+     */
     shouldScan (shouldScan) {
       if (shouldScan) {
         this.$refs.video.play()
@@ -104,24 +150,44 @@ export default {
       }
     },
 
+    /**
+     * Starts continuous decoding process as soon as conditions for that are
+     * fullfilled. The process stops itself automatically when the conditions
+     * are not fullfilled anymore.
+     */
     shouldDecode (shouldDecode) {
       if (shouldDecode) {
         this.keepDecoding()
       }
     },
 
+    /**
+     * Starts continuous locating process as soon as conditions for that are
+     * fullfilled. The process stops itself automatically when the conditions
+     * are not fullfilled anymore.
+     */
     shouldLocate (shouldLocate) {
       if (shouldLocate) {
         this.keepLocating()
       }
     },
 
+    /**
+     * Resets decodeResult when component is un-paused. This way one QR code
+     * can be decoded twice in a row (see #8). The timeout is required to wait
+     * for the video stream to un-freeze. Otherwise the last frame from before
+     * pausing would be rescanned.
+     */
     paused (newValue) {
-      if (!newValue) {
+      if (newValue === false) {
         setTimeout(() => { this.decodeResult = null }, DECODE_INTERVAL)
       }
     },
 
+    /**
+     * When constraints for the used camera change, a new stream has to be
+     * requested. This bootstraps the hole init process again.
+     */
     constraints: {
       deep: true,
 
@@ -131,17 +197,33 @@ export default {
     },
   },
 
+  /**
+   * Instanly requests a stream from the users camera as soon as the component
+   * is mounted. This can't be done in earlier livecycle hooks because it
+   * requires the video and canvas element to be rendered already.
+   */
   mounted () {
     this.$emit('init', this.startCamera())
   },
 
+  /**
+   * If the camera is not released before the component is destroyed, browsers
+   * will indicate that it's still in use and it might be blocked for other
+   * applications.
+   */
   beforeDestroy () {
     this.stopCamera()
   },
 
   methods: {
+    /**
+     * Requests a camera stream using the Stream API and based on
+     * `this.constraints`. Then connects the stream to the video element and
+     * waits for it to load. The promise returned here is the payload for the
+     * `init` event. Any error here leads to this promises rejection.
+     */
     async startCamera () {
-      // check browser support
+      // check if browser is support first
       const canvas = this.$refs.canvas
       if (!(canvas.getContext && canvas.getContext('2d'))) {
         throw new Error('HTML5 Canvas not supported in this browser.')
@@ -182,6 +264,9 @@ export default {
       this.streamLoaded = true
     },
 
+    /**
+     * Releases the current camera stream and resets related instance properties.
+     */
     stopCamera () {
       this.streamLoaded = false
 
@@ -191,9 +276,17 @@ export default {
         )
 
         this.stream = null
+        this.streamWidth = null
+        this.streamHeight = null
       }
     },
 
+    /**
+     * Captures a frame from video stream and draws it to canvas. Then reads
+     * image data from canvas and returns it for further analysis. This extra
+     * step is necessary because it's not possible to read image data from a
+     * video element directly.
+     */
     captureFrame () {
       const video = this.$refs.video
       const canvas = this.$refs.canvas
@@ -206,6 +299,10 @@ export default {
       return ctx.getImageData(...bounds)
     },
 
+    /**
+     * Continuously extracts frames from camera stream and tries to decode
+     * potentially pictured QR codes.
+     */
     keepDecoding () {
       if (this.shouldDecode) {
         const imageData = this.captureFrame()
@@ -217,6 +314,14 @@ export default {
       }
     },
 
+    /**
+     * Continuously extracts frames from camera stream and tries to locate
+     * potentially pictured QR codes.
+     *
+     * The coordinates are based on the original camera resolution but the
+     * video element is responsive and scales with space available. Therefore
+     * the coordinates are re-calculated to be relative to the video element.
+     */
     keepLocating () {
       if (this.shouldLocate) {
         const imageData = this.captureFrame()
@@ -227,13 +332,6 @@ export default {
           if (locationObject === null) {
             this.locateResult = NO_LOCATION
           } else {
-            /*
-             * If stream resolution is greater than available space
-             * the video is scaled down. Detected QR code locateResult is
-             * based on the original resolution though. This difference
-             * is compansated here.
-             */
-
             const video = this.$refs.video
 
             const widthRatio = video.offsetWidth / this.streamWidth
