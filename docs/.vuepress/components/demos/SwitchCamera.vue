f9434f47f74c987d9c75b676804d06539d3ee969
Niklas Gruhn <niklas.gruhn@gmail.com>
2018-11-16 15:43:03 +0100

    update SwitchCamera demo

--- docs/.vuepress/components/demos/SwitchCamera.vue
+++ docs/.vuepress/components/demos/SwitchCamera.vue
@@ -1,35 +1,45 @@
 <template>
-  <qrcode-stream :camera="camera" @init="$emit('init', $event)">
-    <button @click="switchCamera">Switch Camera</button>
-  </qrcode-stream>
+  <div>
+    <p class="error" v-if="noFrontCamera">
+      You don't seem to have a front camera on your device
+    </p>
+
+    <qrcode-stream :camera="{ facingMode }" @init="onInit">
+      <button @click="switchCamera">Switch Camera</button>
+    </qrcode-stream>
+  </div>
 </template>
 
 <script>
+const REAR_CAMERA = { ideal: 'environment' }
+const FRONT_CAMERA = { exact: 'user' }
+
 export default {
   data () {
     return {
-      useRearCamera: true
+      facingMode: REAR_CAMERA,
+      noFrontCamera: false
     }
-  },
-
-  computed: {
-    camera () {
-      if (this.useRearCamera) {
-        return {
-          facingMode: { ideal: 'environment' }
-        }
-      } else {
-        return {
-          facingMode: { exact: 'user' }
-        }
-      }
-    }
-
   },
 
   methods: {
     switchCamera () {
-      this.useRearCamera = !this.useRearCamera
+      if (this.facingMode === FRONT_CAMERA) {
+        this.facingMode = REAR_CAMERA
+      } else {
+        this.facingMode = FRONT_CAMERA
+      }
+    },
+
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        this.noFrontCamera = this.facingMode === FRONT_CAMERA
+          && error.name === 'OverconstrainedError'
+
+        console.error(error)
+      }
     }
   }
 }
@@ -40,5 +50,10 @@ button {
   position: absolute;
   left: 10px;
   top: 10px;
+}
+
+.error {
+  color: red;
+  font-weight: bold;
 }
 </style>
