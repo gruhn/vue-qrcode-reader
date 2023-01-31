8285c2ba927c1c01eb9e6c65d0b4ecae37030740
Viacheslav Domin <v.dyomin@gmail.com>
2023-02-01 00:00:27 +0200

    fix(camera): Desk View Camera was added into blacklist

--- src/misc/camera.js
+++ src/misc/camera.js
@@ -30,7 +30,7 @@ class Camera {
 // media constraints don't allow us to specify which camera we want exactly.
 const narrowDownFacingMode = async camera => {
   // Filter some devices, known to be bad choices.
-  const deviceBlackList = ["OBS Virtual Camera", "OBS-Camera"];
+  const deviceBlackList = ["OBS Virtual Camera", "OBS-Camera", "Desk View Camera"];
 
   const devices = (await navigator.mediaDevices.enumerateDevices())
     .filter(({ kind }) => kind === "videoinput")
