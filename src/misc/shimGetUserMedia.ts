4a77f825889aee546230ab67a66120a53f8513ce
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-06-07 04:20:41 +0200

    fix(shim): readd custom imports

--- src/misc/shimGetUserMedia.ts
+++ src/misc/shimGetUserMedia.ts
@@ -1,15 +1,27 @@
-import adapter from 'webrtc-adapter'
+// @ts-ignore
+import { shimGetUserMedia as chromeShim } from 'webrtc-adapter/src/js/chrome/getusermedia'
+// @ts-ignore
+import { shimGetUserMedia as firefoxShim } from 'webrtc-adapter/src/js/firefox/getusermedia'
+// @ts-ignore
+import { shimGetUserMedia as safariShim } from 'webrtc-adapter/src/js/safari/safari_shim'
+// @ts-ignore
+import { detectBrowser } from 'webrtc-adapter/src/js/utils'
 
 import { StreamApiNotSupportedError } from './errors'
 import { indempotent } from './util'
 
 export default indempotent(() => {
-  const browser = adapter.browserDetails.browser
+  const result = detectBrowser(window)
 
-  switch (browser) {
+  switch (result.browser) {
     case 'chrome':
+      chromeShim(window, result)
+      break
     case 'firefox':
+      firefoxShim(window, result)
+      break
     case 'safari':
+      safariShim(window, result)
       break
     default:
       throw new StreamApiNotSupportedError()
