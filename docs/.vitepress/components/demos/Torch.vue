e2e95396d5b0e4215fc6d17e93af1d9470918d55
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-10-17 00:26:03 +0800

    chore(format): update format settings

--- docs/.vitepress/components/demos/Torch.vue
+++ docs/.vitepress/components/demos/Torch.vue
@@ -3,24 +3,39 @@
     <p>
       Pick camera:
       <select v-model="selected">
-        <option v-for="device in devices" :key="device.label" :value="device">
+        <option
+          v-for="device in devices"
+          :key="device.label"
+          :value="device"
+        >
           {{ device.label }}
         </option>
       </select>
     </p>
 
-    <p v-if="torchNotSupported" class="error">Torch not supported for active camera</p>
+    <p
+      v-if="torchNotSupported"
+      class="error"
+    >
+      Torch not supported for active camera
+    </p>
 
-    <qrcode-stream 
-      :torch="torchActive" 
-      :constraints="{ deviceId: selected.deviceId }" 
-      v-if="selected !== null"       
+    <qrcode-stream
+      :torch="torchActive"
+      :constraints="{ deviceId: selected.deviceId }"
+      v-if="selected !== null"
       @error="console.error"
       @camera-on="onCameraOn"
       v-memo="[torchActive, selected.deviceId]"
     >
-      <button @click="torchActive = !torchActive" :disabled="torchNotSupported">
-        <img :src="withBase(icon)" alt="toggle torch" />
+      <button
+        @click="torchActive = !torchActive"
+        :disabled="torchNotSupported"
+      >
+        <img
+          :src="withBase(icon)"
+          alt="toggle torch"
+        />
       </button>
     </qrcode-stream>
   </div>
@@ -35,21 +50,22 @@ const selected = ref(null as MediaDeviceInfo | null)
 const devices = ref([] as MediaDeviceInfo[])
 
 onMounted(async () => {
-  devices.value = (await navigator.mediaDevices.enumerateDevices())
-    .filter(({ kind }) => kind === 'videoinput')
+  devices.value = (await navigator.mediaDevices.enumerateDevices()).filter(
+    ({ kind }) => kind === 'videoinput'
+  )
 
-	if (devices.value.length > 0) {
-		selected.value = devices.value[0]	
-	}
+  if (devices.value.length > 0) {
+    selected.value = devices.value[0]
+  }
 })
 
 const torchActive = ref(false)
 const torchNotSupported = ref(false)
 
 const icon = computed(() => {
-  if (torchActive.value) { 
+  if (torchActive.value) {
     return '/flash-off.svg'
-  } else { 
+  } else {
     return '/flash-on.svg'
   }
 })
@@ -59,7 +75,7 @@ function onCameraOn(capabilities) {
   torchNotSupported.value = !capabilities.torch
 }
 
-function onError(err) { 
+function onError(err) {
   console.error(err)
 }
 </script>
