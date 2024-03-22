694b33bd71e81264f41e9900de9f95f4ffa98eaf
carpONE <81232339+carpONE@users.noreply.github.com>
2024-03-23 01:25:38 +0800

    feat：support formats (#414)

--- src/components/QrcodeCapture.vue
+++ src/components/QrcodeCapture.vue
@@ -16,12 +16,19 @@ import CommonAPI from "../mixins/CommonAPI.vue";
 export default {
   name: "qrcode-capture",
 
+  props:{
+    formats: {
+      type: Array,
+      default: () => ["qr_code"]
+    }
+  },
+
   mixins: [CommonAPI],
 
   methods: {
     onChangeInput(event) {
       const files = [...event.target.files];
-      const resultPromises = files.map(processFile);
+      const resultPromises = files.map(file => processFile(file, this.formats));
 
       resultPromises.forEach(this.onDetect);
     }
