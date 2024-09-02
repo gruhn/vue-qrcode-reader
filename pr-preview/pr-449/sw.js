73d439ab17c3e4d5836b7443c84b83286067da07
gruhn <gruhn@users.noreply.github.com>
2024-09-02 11:15:49 +0000

    Deploy preview for PR 449 🛫

---
+++ pr-preview/pr-449/sw.js
@@ -0,0 +1,330 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-5199072c'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "404.html",
+    "revision": "0d85dd8b0275491d1b4fb89a04cc8892"
+  }, {
+    "url": "api/QrcodeCapture.html",
+    "revision": "b027b9f377de6add75aab32f2a0fdb2f"
+  }, {
+    "url": "api/QrcodeDropZone.html",
+    "revision": "6ca17feae2a2b2dc578190e304ad5580"
+  }, {
+    "url": "api/QrcodeStream.html",
+    "revision": "bd5d8ba757fcf7d26ba3d08cd8d52493"
+  }, {
+    "url": "assets/api_QrcodeCapture.md.BVomH-fp.js",
+    "revision": null
+  }, {
+    "url": "assets/api_QrcodeCapture.md.BVomH-fp.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/api_QrcodeDropZone.md.CqxuZhVd.js",
+    "revision": null
+  }, {
+    "url": "assets/api_QrcodeDropZone.md.CqxuZhVd.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/api_QrcodeStream.md.CGQgF4oK.js",
+    "revision": null
+  }, {
+    "url": "assets/api_QrcodeStream.md.CGQgF4oK.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/app.DzN9cVpk.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/@localSearchIndexroot.BQqEPrZR.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/camera.BJQyjlnn.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/framework.BtL3uYYi.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/QrcodeStream.vue_vue_type_script_setup_true_lang.CUHYp3xr.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/safari_32x32.Bkuv9jEj.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/theme.C1T0t5Zq.js",
+    "revision": null
+  }, {
+    "url": "assets/chunks/VPLocalSearchBox.BPLfKR6Q.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_DragDrop.md.12E-B_I3.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_DragDrop.md.12E-B_I3.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_FullDemo.md.EA-azuP_.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_FullDemo.md.EA-azuP_.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Fullscreen.md.CMW7y2Ip.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Fullscreen.md.CMW7y2Ip.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_LoadingIndicator.md.BVZFqNj1.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_LoadingIndicator.md.BVZFqNj1.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.M3YU2Vzr.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.M3YU2Vzr.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Simple.md.CWiqK3J_.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Simple.md.CWiqK3J_.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_SwitchCamera.md.B-obcuEB.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_SwitchCamera.md.B-obcuEB.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Torch.md.AnpZuZA8.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Torch.md.AnpZuZA8.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Upload.md.Ds3GTY8o.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Upload.md.Ds3GTY8o.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Validate.md.Ba0B0yOc.js",
+    "revision": null
+  }, {
+    "url": "assets/demos_Validate.md.Ba0B0yOc.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.DnOK2cmZ.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.DnOK2cmZ.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic-ext.5XJwZIOp.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic.D6csxwjC.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek-ext.CHOfFY1k.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek.9J96vYpw.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin-ext.BGcWXLrn.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin.DbsTr1gm.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-vietnamese.DHNAd7Wr.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic-ext.DxP3Awbn.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic.CMhn1ESj.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek-ext.D0mI3NpI.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek.JvnBZ4YD.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin-ext.ZlYT4o7i.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin.Bu8hRsVA.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-vietnamese.ClpjcLMQ.woff2",
+    "revision": null
+  }, {
+    "url": "assets/style.DtBLy4Td.css",
+    "revision": null
+  }, {
+    "url": "camera-switch.svg",
+    "revision": "c966900237eef848d4aeb18b0ad64371"
+  }, {
+    "url": "checkmark.svg",
+    "revision": "398fc16c5cbd6c20b529b76742c33942"
+  }, {
+    "url": "debug-memory-leak.html",
+    "revision": "baffbefe1bde1d10f0c089b20f0cb9ed"
+  }, {
+    "url": "demos/DragDrop.html",
+    "revision": "54ad4ea7a7bcba4538fdd84736ff761e"
+  }, {
+    "url": "demos/FullDemo.html",
+    "revision": "22ad2d394e5e6001bf69f80d7c9a35db"
+  }, {
+    "url": "demos/Fullscreen.html",
+    "revision": "b610a549bbe27efdadb7fd854166412b"
+  }, {
+    "url": "demos/LoadingIndicator.html",
+    "revision": "316dcf6ef232d95a7ca0903c04f23da6"
+  }, {
+    "url": "demos/ScanSameQrcodeMoreThanOnce.html",
+    "revision": "c18dcc31f749e2032301386eed108466"
+  }, {
+    "url": "demos/Simple.html",
+    "revision": "3067aea3c4f4f4fc4c3d297e770ecd51"
+  }, {
+    "url": "demos/SwitchCamera.html",
+    "revision": "83295d03a34eb3e87620e83c35ea6aa8"
+  }, {
+    "url": "demos/Torch.html",
+    "revision": "01564732ec52b81e5a37612c6b3bdc49"
+  }, {
+    "url": "demos/Upload.html",
+    "revision": "65baf704deb5f4f21ffa72b30b6a9e05"
+  }, {
+    "url": "demos/Validate.html",
+    "revision": "b09f32e3eb9fbb66ac485c3d530dd8dd"
+  }, {
+    "url": "flash-off.svg",
+    "revision": "8b05f5dcd6712992a544b34520ec7262"
+  }, {
+    "url": "flash-on.svg",
+    "revision": "23580871877110ec5e7dcd41efdbd07b"
+  }, {
+    "url": "fullscreen-exit.svg",
+    "revision": "77f8bddd41a7894d1a00324ed9dcb8f9"
+  }, {
+    "url": "fullscreen.svg",
+    "revision": "432c44f09de0b4e0f9e236fad9b8c7f9"
+  }, {
+    "url": "index.html",
+    "revision": "adbc629e07dea3bff557278f49f78ddb"
+  }, {
+    "url": "logo.png",
+    "revision": "5f0c1d6358641bc48207acb9fa0b6182"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "05431c417219f6c247a23488366a2b41"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "5e0a4893ebdd02af95cf73c7b7759ddd"
+  }, {
+    "url": "registerSW.js",
+    "revision": "cb826733d1228e0f33c8117077d32d72"
+  }, {
+    "url": "select-camera-demo.html",
+    "revision": "caa12e13f97000bc06e565e64bb8bd0f"
+  }, {
+    "url": "simple-demo.html",
+    "revision": "d2e0337bc830c36ac24089933500e897"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "05431c417219f6c247a23488366a2b41"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "5e0a4893ebdd02af95cf73c7b7759ddd"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "cdc5a0b0c58ffb38b8b281141584d834"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
