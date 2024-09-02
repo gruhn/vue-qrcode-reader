aa7c5aa7a4c1837c4a33f38e4df159ccca0e0b9b
Niklas Gruhn <niklas@gruhn.me>
2024-09-02 13:30:36 +0200

    perf: BarcodeDetector polyfill only if no native support
    Still using polyfill for `QrcodeDropZone` and `QrcodeCapture` even on
    platforms with native `BarcodeDetector` is available, because on some
    of them `BarcodeDetector.detect` does not support `Blob` / `File`
    inputs.
    
    See: #447

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -2,7 +2,7 @@
  * Takes a function `action` and returns a new function, that behaves
  * like action but when called a second time does nothing.
  */
-export const indempotent = <T>(action: (x: any) => T) => {
+export const idempotent = <T>(action: (x: any) => T) => {
   let called = false
   let result: T | undefined = undefined
 
