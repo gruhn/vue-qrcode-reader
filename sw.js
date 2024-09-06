6a358acae14471317e468b7fd3e4cefeac84f870
gruhn <niklas@gruhn.me>
2024-09-06 18:59:58 +0000

    Deploying to gh-pages from @ gruhn/vue-qrcode-reader@aa7c5aa7a4c1837c4a33f38e4df159ccca0e0b9b 🚀

--- sw.js
+++ sw.js
@@ -79,16 +79,16 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "404.html",
-    "revision": "25c653da4b39a417f75dc1656946ad2b"
+    "revision": "65385983a18c2ceaa705a073cefd4cdc"
   }, {
     "url": "api/QrcodeCapture.html",
-    "revision": "2f183f76ac67db13d5c9e1d48b2ffd86"
+    "revision": "310539ff229f4f6d70726178f0f45ea9"
   }, {
     "url": "api/QrcodeDropZone.html",
-    "revision": "6b11bc1aa316366a993815cdd121e834"
+    "revision": "7bf5d228dcf01a27514339e3fffe49fc"
   }, {
     "url": "api/QrcodeStream.html",
-    "revision": "842cf5d3f96187a68f27f344f0e3456d"
+    "revision": "5b5d2dcd2f1952f5a62121cb928a065d"
   }, {
     "url": "assets/api_QrcodeCapture.md.C7iqouC7.js",
     "revision": null
@@ -108,58 +108,58 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "url": "assets/api_QrcodeStream.md.DnuBzp6K.lean.js",
     "revision": null
   }, {
-    "url": "assets/app.9z4OoTxy.js",
+    "url": "assets/app.M3x9aKvg.js",
     "revision": null
   }, {
     "url": "assets/chunks/@localSearchIndexroot.DBcb1Hh6.js",
     "revision": null
   }, {
-    "url": "assets/chunks/camera.C5yEuDPB.js",
+    "url": "assets/chunks/camera.DE4cteSh.js",
     "revision": null
   }, {
     "url": "assets/chunks/framework.BwCxet3-.js",
     "revision": null
   }, {
-    "url": "assets/chunks/QrcodeStream.vue_vue_type_script_setup_true_lang.DEKLPwsC.js",
+    "url": "assets/chunks/QrcodeStream.vue_vue_type_script_setup_true_lang.DspH0Vrb.js",
     "revision": null
   }, {
     "url": "assets/chunks/safari_32x32.Bkuv9jEj.js",
     "revision": null
   }, {
-    "url": "assets/chunks/theme.B-e95pFv.js",
+    "url": "assets/chunks/theme.s67tei5V.js",
     "revision": null
   }, {
-    "url": "assets/chunks/VPLocalSearchBox.C6Etvfbf.js",
+    "url": "assets/chunks/VPLocalSearchBox.Ci3nu08r.js",
     "revision": null
   }, {
-    "url": "assets/demos_DragDrop.md.fcrZg2Fe.js",
+    "url": "assets/demos_DragDrop.md.KueMX9qx.js",
     "revision": null
   }, {
-    "url": "assets/demos_DragDrop.md.fcrZg2Fe.lean.js",
+    "url": "assets/demos_DragDrop.md.KueMX9qx.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_FullDemo.md.jmkwVxvJ.js",
+    "url": "assets/demos_FullDemo.md.dj7uxa22.js",
     "revision": null
   }, {
-    "url": "assets/demos_FullDemo.md.jmkwVxvJ.lean.js",
+    "url": "assets/demos_FullDemo.md.dj7uxa22.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_Fullscreen.md.sCAwa0lj.js",
+    "url": "assets/demos_Fullscreen.md.D5kCBhsJ.js",
     "revision": null
   }, {
-    "url": "assets/demos_Fullscreen.md.sCAwa0lj.lean.js",
+    "url": "assets/demos_Fullscreen.md.D5kCBhsJ.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_LoadingIndicator.md.BqFycQZ_.js",
+    "url": "assets/demos_LoadingIndicator.md.BPlhc5KO.js",
     "revision": null
   }, {
-    "url": "assets/demos_LoadingIndicator.md.BqFycQZ_.lean.js",
+    "url": "assets/demos_LoadingIndicator.md.BPlhc5KO.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.D7rzlybC.js",
+    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.Brm_Gomp.js",
     "revision": null
   }, {
-    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.D7rzlybC.lean.js",
+    "url": "assets/demos_ScanSameQrcodeMoreThanOnce.md.Brm_Gomp.lean.js",
     "revision": null
   }, {
     "url": "assets/demos_Simple.md.BCBYY3fY.js",
@@ -168,28 +168,28 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "url": "assets/demos_Simple.md.BCBYY3fY.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_SwitchCamera.md.QGG2W18j.js",
+    "url": "assets/demos_SwitchCamera.md.CAvxezaF.js",
     "revision": null
   }, {
-    "url": "assets/demos_SwitchCamera.md.QGG2W18j.lean.js",
+    "url": "assets/demos_SwitchCamera.md.CAvxezaF.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_Torch.md.R6bRlqfK.js",
+    "url": "assets/demos_Torch.md.kskOygTt.js",
     "revision": null
   }, {
-    "url": "assets/demos_Torch.md.R6bRlqfK.lean.js",
+    "url": "assets/demos_Torch.md.kskOygTt.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_Upload.md.BO0OFwB_.js",
+    "url": "assets/demos_Upload.md.Dx4hnZdl.js",
     "revision": null
   }, {
-    "url": "assets/demos_Upload.md.BO0OFwB_.lean.js",
+    "url": "assets/demos_Upload.md.Dx4hnZdl.lean.js",
     "revision": null
   }, {
-    "url": "assets/demos_Validate.md.DC1csbFk.js",
+    "url": "assets/demos_Validate.md.hjIDCaLb.js",
     "revision": null
   }, {
-    "url": "assets/demos_Validate.md.DC1csbFk.lean.js",
+    "url": "assets/demos_Validate.md.hjIDCaLb.lean.js",
     "revision": null
   }, {
     "url": "assets/index.md.HnahRfcr.js",
@@ -240,7 +240,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "url": "assets/inter-roman-vietnamese.ClpjcLMQ.woff2",
     "revision": null
   }, {
-    "url": "assets/style.BcK_-s6q.css",
+    "url": "assets/style.DPXyhsKt.css",
     "revision": null
   }, {
     "url": "camera-switch.svg",
@@ -253,34 +253,34 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "baffbefe1bde1d10f0c089b20f0cb9ed"
   }, {
     "url": "demos/DragDrop.html",
-    "revision": "b293a3b37f4628a9cd3050ddedcc4e10"
+    "revision": "fb027ef29737d0c11b563824bf5bb8bd"
   }, {
     "url": "demos/FullDemo.html",
-    "revision": "69e519cf5dbdf6c296b265d1f69a6be9"
+    "revision": "49e07f83498c9ea905203ddab5c5b407"
   }, {
     "url": "demos/Fullscreen.html",
-    "revision": "85d71bdbfea9b326e81daebedf650f39"
+    "revision": "eb8c5ce049e22fdd9eae9f4a99bc7758"
   }, {
     "url": "demos/LoadingIndicator.html",
-    "revision": "0d7350f89ce4b76a44fa016c931e3e87"
+    "revision": "efd5e7a831ae849226d7ebde5a5edefc"
   }, {
     "url": "demos/ScanSameQrcodeMoreThanOnce.html",
-    "revision": "b9ba270e0afa59b7355b07b3bcbd42c1"
+    "revision": "671ae000ebdd76dbb2f96f5ef5f30eea"
   }, {
     "url": "demos/Simple.html",
-    "revision": "eec9e70b0c51077295e04d57e58dd7ac"
+    "revision": "15a91e7b813a826c45bce29daa021171"
   }, {
     "url": "demos/SwitchCamera.html",
-    "revision": "3b72af2fe45b4e00c7e0a4a7be54f1e3"
+    "revision": "ec7874588c5c9bad5a1993d360408ad9"
   }, {
     "url": "demos/Torch.html",
-    "revision": "8c0ccad9b7cd23541dda8578fbbe0df9"
+    "revision": "24c2ba1cc3de272b4156cabb69c43b73"
   }, {
     "url": "demos/Upload.html",
-    "revision": "3918335cfa9a89d21eae876e616db6a4"
+    "revision": "805be3f5cbc91ca2f03b6d56c6fafbf7"
   }, {
     "url": "demos/Validate.html",
-    "revision": "c461500ca11f9b6fbaac854ad96dd965"
+    "revision": "73f90716de62449dfa0f16a21071f6ff"
   }, {
     "url": "flash-off.svg",
     "revision": "8b05f5dcd6712992a544b34520ec7262"
@@ -295,7 +295,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "432c44f09de0b4e0f9e236fad9b8c7f9"
   }, {
     "url": "index.html",
-    "revision": "94f044612e84db523e4a6ab12b4929ce"
+    "revision": "c438332fe3ac73e3d0950579f6cbc4a5"
   }, {
     "url": "logo.png",
     "revision": "5f0c1d6358641bc48207acb9fa0b6182"
