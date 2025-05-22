f41fd88d7d36a6048d26098c8bbe60da7de35e9d
ifyoung <1558528975@qq.com>
2025-05-22 15:59:33 +0800

    lib ver fixed And export setZXingModuleOverrides

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import QrcodeStream from "./components/QrcodeStream.vue";
 import QrcodeCapture from "./components/QrcodeCapture.vue";
 import QrcodeDropZone from "./components/QrcodeDropZone.vue";
 
+import { setZXingModuleOverrides } from 'barcode-detector'
+
 // Install the components
 export function install(Vue) {
   Vue.component("qrcode-stream", QrcodeStream);
@@ -10,7 +12,7 @@ export function install(Vue) {
 }
 
 // Expose the components
-export { QrcodeStream, QrcodeCapture, QrcodeDropZone };
+export { QrcodeStream, QrcodeCapture, QrcodeDropZone,setZXingModuleOverrides };
 
 /* -- Plugin definition & Auto-install -- */
 /* You shouldn't have to modify the code below */
