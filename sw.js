1597bab990773a8baffcabdb3da635679e77409e
gruhn <niklas@gruhn.me>
2025-07-23 11:14:34 +0000

    Deploying to gh-pages from @ gruhn/vue-qrcode-reader@ce28111a7390c3ccb0482bcd9f949adfeafdff72 🚀

--- sw.js
+++ sw.js
@@ -79,16 +79,16 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "404.html",
-    "revision": "2c84ab7824d369be73d7cc119a6ce07a"
+    "revision": "b74b59d248b3f3a7fd444a12c83c65ed"
   }, {
     "url": "api/QrcodeCapture.html",
-    "revision": "238b7a3f225655171329910904aa7226"
+    "revision": "6639ae0a1a8daf0a51d027962cb0ae8a"
   }, {
     "url": "api/QrcodeDropZone.html",
-    "revision": "aeee77ed386370399591c7f3790dfe9e"
+    "revision": "c308ec7bf5267aeb3a91fd209ebe636b"
   }, {
     "url": "api/QrcodeStream.html",
-    "revision": "f5368cb75cc483718f520e72a7b1cd90"
+    "revision": "582fd730ce93fc4a583cddc82267b090"
   }, {
     "url": "assets/api_QrcodeCapture.md.B5QU-yV0.js",
     "revision": null
@@ -108,7 +108,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "url": "assets/api_QrcodeStream.md.9CJzVv4z.lean.js",
     "revision": null
   }, {
-    "url": "assets/app.BmUUpjO_.js",
+    "url": "assets/app.4MlHmAg2.js",
     "revision": null
   }, {
     "url": "assets/chunks/@localSearchIndexroot.BiSJWzvQ.js",
@@ -126,10 +126,10 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "url": "assets/chunks/safari_32x32.Bkuv9jEj.js",
     "revision": null
   }, {
-    "url": "assets/chunks/theme.Dca_P1w6.js",
+    "url": "assets/chunks/theme.TxNFPanC.js",
     "revision": null
   }, {
-    "url": "assets/chunks/VPLocalSearchBox.CdDc-K49.js",
+    "url": "assets/chunks/VPLocalSearchBox.DUe8aDgT.js",
     "revision": null
   }, {
     "url": "assets/demos_DragDrop.md.BtW0kG8T.js",
@@ -240,8 +240,11 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "url": "assets/inter-roman-vietnamese.ClpjcLMQ.woff2",
     "revision": null
   }, {
-    "url": "assets/style.CAqy5f8a.css",
+    "url": "assets/style._o-JA5m5.css",
     "revision": null
+  }, {
+    "url": "barcode-detector-test.html",
+    "revision": "c6ab43ddd912cedd13ceae46427c275d"
   }, {
     "url": "camera-switch.svg",
     "revision": "c966900237eef848d4aeb18b0ad64371"
@@ -253,34 +256,34 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "baffbefe1bde1d10f0c089b20f0cb9ed"
   }, {
     "url": "demos/DragDrop.html",
-    "revision": "2fb465faf3e614d8117dfb111becbb26"
+    "revision": "78719fa896c1334ef5e8988313af4b09"
   }, {
     "url": "demos/FullDemo.html",
-    "revision": "6d3dc44b06ac43997195eb88bcc8b1cf"
+    "revision": "c9d13fb770f2adbdf3de954ff43cbf25"
   }, {
     "url": "demos/Fullscreen.html",
-    "revision": "dd9f9b57e79f5458f1973694d253dc8e"
+    "revision": "96d988f3710a0bc233b0dd23f35c425d"
   }, {
     "url": "demos/LoadingIndicator.html",
-    "revision": "21b3335a3329d730945b9497aa740d1c"
+    "revision": "774ef66c6dbdf62edc8c4941fc9c2afe"
   }, {
     "url": "demos/ScanSameQrcodeMoreThanOnce.html",
-    "revision": "e3abd4fc75bda94f0404ec7ccc2f5a21"
+    "revision": "816d5a966a40e0c47c79a7d7565b1eae"
   }, {
     "url": "demos/Simple.html",
-    "revision": "d2000ab67d0083bf36a6a30108ea5948"
+    "revision": "4b7a40f7efaf1325333d44734eeb352f"
   }, {
     "url": "demos/SwitchCamera.html",
-    "revision": "6f9744a15787e9de2bf9a3566a1cd99c"
+    "revision": "149a1d5b462aa4558123ec10c00dcd39"
   }, {
     "url": "demos/Torch.html",
-    "revision": "a9f6a2e1a17b7e67b0a572200d3f4d90"
+    "revision": "5a4251bbc95418bdc2092da2e27f384d"
   }, {
     "url": "demos/Upload.html",
-    "revision": "3ce69085195f42354557c7a76defe598"
+    "revision": "ff34910141f1910bbe64dfa6a97b4715"
   }, {
     "url": "demos/Validate.html",
-    "revision": "4a9eef3876a15320e1debe2057d207d3"
+    "revision": "a4046345d8050d7d034188e6b1d01480"
   }, {
     "url": "flash-off.svg",
     "revision": "8b05f5dcd6712992a544b34520ec7262"
@@ -295,7 +298,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "432c44f09de0b4e0f9e236fad9b8c7f9"
   }, {
     "url": "index.html",
-    "revision": "d3be8f8b78251459499fcdf84e7dfbed"
+    "revision": "359183b4d59528bfff13e681a6387a02"
   }, {
     "url": "logo.png",
     "revision": "5f0c1d6358641bc48207acb9fa0b6182"
